59896e51e5c6703e72b2dde3ecaed1872229a0ea
Lucas Anjos <oargus.g@gmail.com>
2024-08-07 19:46:43 -0300

    Tela de boas vindas, barra de busca

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,35 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />
+    // Carrega as fontes necessárias para o aplicativo
+    // O useFonts é um hook do expo-font que carrega as
+    // fontes e retorna um array com duas posições
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+
+    // A função abaixo serve para esconder a tela de splash assim que as fontes forem carregadas
+    // Isso é importante para que o usuário não veja a tela de splash por muito tempo
+    // A função useCallback é uma função que é executada apenas uma vez, e não a cada renderização
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    // Se as fontes não foram carregadas, retorna null para não renderizar nada
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return <Stack onLayout={onLayoutRootView} />
 }
 
 export default Layout;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,73 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    Image,
+    FlatList
+} from 'react-native';
+import { useRouter } from 'expo-router';
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const jobTypes = ["Integral", "Meio Periodo", "Freelancer", "Estagio", "Temporario"];
 
 const Welcome = () => {
-  return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
-  )
+    const router = useRouter();
+    const [activeJobType, setActiveJobType] = useState("Integral");
+
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text style={styles.userName}>Hello Lucas</Text>
+                <Text style={styles.welcomeMessage}>Find yout perfect Job</Text>
+            </View>
+
+            <View style={styles.searchContainer}>
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        style={styles.searchInput}
+                        value=""
+                        onChange={() => {}}
+                        placeholder='O que está procurando?'
+                    />
+                </View>
+
+                <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+                    <Image 
+                        source={icons.search} 
+                        resizeMode="contain"
+                        style={styles.searchBtnImage}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.tabsContainer}>
+                {/* Renderiza uma lista com todos os itens do array */}
+                <FlatList 
+                    data={jobTypes}
+                    // Renderiza cada item do array com o componente TouchableOpacity
+                    renderItem={({ item }) => (
+                        <TouchableOpacity 
+                            style={styles.tab(activeJobType, item)} 
+                            onPress={() => {
+                                setActiveJobType(item);
+                                router.push(`/search/${item}`);
+                            }}
+                        >
+                            {/* Aqui podemos passar o item e o tipo ativo para ser usado no estilo */}
+                            <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={item => item}
+                    contentContainerStyle={{ columnGap: SIZES.small }}
+                    horizontal
+                />
+            </View>
+        </View>
+    )
 }
 
 export default Welcome
