2814a6d167aaeba428d70cfde179cffafe4a4f7d
Lucas Anjos <oargus.g@gmail.com>
2024-08-07 21:31:53 -0300

    useFetch and Popular Jobs

---
+++ hook/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetch = (endpoint, query) => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'x-rapidapi-key': "271bd593admsh4d2a3400bcc231dp1c7679jsncdf0835f74fb",
+            'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+        },
+        params: { ...query }
+    };
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options);
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+            alert("Algo deu errado");
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    // Executa a função fetchData() apenas uma vez
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+    return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
